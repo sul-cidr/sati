dc8ff619b8a1137f3208bcb0db46f26a7c7b2c22
Simon Wiles <simonjwiles@gmail.com>
2020-06-18 12:34:20 -0700

    Update model

--- sati/items/models.py
+++ sati/items/models.py
@@ -1,18 +1,31 @@
-from django.contrib.postgres.fields import JSONField
+from django.contrib.postgres.fields import ArrayField, JSONField
 from django.db import models
+
+
+class ItemFormat(models.TextChoices):
+    MC = "MC", "MC"
+    CR = "CR", "CR"
+    FB = "FB", "FB"
+    FT = "FT", "FT"
+    FI = "FI", "FI"
+
+
+class ItemOrigin(models.Model):
+    origin = JSONField()
+
+
+class Item(models.Model):
+    item_id = models.CharField(max_length=30, unique=True)
+    name = models.CharField(max_length=30)
+    format = ArrayField(models.CharField(max_length=2, choices=ItemFormat.choices))
+    # origin = models.ForeignKey(ItemOrigin, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.item_id} ({self.name.title()})"
 
 
 class ItemCoding(models.Model):
     coding_source = models.CharField(max_length=30)
     coding_scheme = models.CharField(max_length=30)
     coding = JSONField()
-
-
-class ItemOrigin(models.Model):
-    item_id = models.CharField(max_length=30)
-    other_ids = JSONField()
-    origin = JSONField()
-
-
-class Item(models.Model):
-    origin = models.ForeignKey(ItemOrigin, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
