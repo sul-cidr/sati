4b16522c78f7457f029e54b71f9d9ba44cc91c44
Simon Wiles <simonjwiles@gmail.com>
2020-06-17 17:43:18 -0700

    Initial model

--- sati/items/models.py
+++ sati/items/models.py
@@ -1,3 +1,18 @@
-from django.db import models  # noqa
+from django.contrib.postgres.fields import JSONField
+from django.db import models
 
-# Create your models here.
+
+class ItemCoding(models.Model):
+    coding_source = models.CharField(max_length=30)
+    coding_scheme = models.CharField(max_length=30)
+    coding = JSONField()
+
+
+class ItemOrigin(models.Model):
+    item_id = models.CharField(max_length=30)
+    other_ids = JSONField()
+    origin = JSONField()
+
+
+class Item(models.Model):
+    origin = models.ForeignKey(ItemOrigin, on_delete=models.CASCADE)
