942c40ad048c6c0a65ad8cc8470667e92a218b34
Simon Wiles <simonjwiles@gmail.com>
2020-06-18 21:14:46 -0700

    Add an index page

--- sati/urls.py
+++ sati/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from baton.autodiscover import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("baton/", include("baton.urls")),
+    path("", TemplateView.as_view(template_name="index.html")),
 ]
