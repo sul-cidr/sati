f7a9f615ce08b306239291637e4da54e5c9caf28
Simon Wiles <simonjwiles@gmail.com>
2020-06-19 13:09:47 -0700

    Reconfigure static assets a little

--- sati/settings.py
+++ sati/settings.py
@@ -114,9 +114,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-# STATIC_ROOT = os.getenv("STATIC_ROOT", "static")
+STATIC_ROOT = os.getenv("STATIC_ROOT", os.path.join(BASE_DIR, "static"))
 STATIC_URL = os.getenv("STATIC_URL", "/static/")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "assets")]
 
 
 ADMINS = (
