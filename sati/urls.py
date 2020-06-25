67c8cf3e090feb616b72504bc35ee727db64a389
Simon Wiles <simonjwiles@gmail.com>
2020-06-25 16:13:48 -0700

    Serve media files properly in dev mode
    This will probably need to be changed at some point, but it will do for now.

--- sati/urls.py
+++ sati/urls.py
@@ -12,3 +12,10 @@ urlpatterns = [
     path("baton/", include("baton.urls")),
     path("", TemplateView.as_view(template_name="index.html")),
 ]
+
+
+from django.conf import settings  # noqa: E402
+from django.conf.urls.static import static  # noqa: E402
+
+if settings.DEBUG is True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
