03404e58de0619093fbef04b2f1b803bc3ac527a
Simon Wiles <simonjwiles@gmail.com>
2020-06-26 13:18:18 -0700

    Set timezone on all requests

--- sati/settings.py
+++ sati/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "sati.middleware.TimezoneMiddleware",
 ]
 
 if DEBUG:
