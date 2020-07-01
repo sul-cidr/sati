9eb56064b900ad45af65fbf489ca2859acc7b10d
Simon Wiles <simonjwiles@gmail.com>
2020-06-30 21:12:26 -0700

    Apply AdminPagedownWidget to TextFields on ItemAdmin

--- sati/items/admin.py
+++ sati/items/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.db import models
 
+from pagedown.widgets import AdminPagedownWidget
 from sati.items.models import Item, ItemCoding, ItemFormat
 
 
@@ -25,6 +27,10 @@ class ItemFormatFilter(admin.SimpleListFilter):
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.TextField: {"widget": AdminPagedownWidget},
+    }
+
     list_display = ("item_id", "name", "content_area", "format", "requires_attention")
     list_filter = ("requires_attention", "content_area", ItemFormatFilter)
     search_fields = ("item_id", "name")
