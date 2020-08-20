c1804bcebdd7bf38214547ff20a279bae3a86502
Simon Wiles <simonjwiles@gmail.com>
2020-08-19 17:32:19 -0700

    Add a generic JSONSchemaField
    This field requires the schema to be hard-coded at time of instantiation, so will need to be adapted.

--- sati/items/fields.py
+++ sati/items/fields.py
@@ -1,5 +1,13 @@
+import json
+from pathlib import Path
+
+from jsonschema import validate, exceptions as jsonschema_exceptions
+
 from django import forms
+from django.conf import settings
+from django.core import exceptions
 from django.contrib.postgres.fields import ArrayField
+from django.contrib.postgres.fields import JSONField
 
 from .widgets import ArraySelectMultiple
 
@@ -14,3 +22,35 @@ class ChoiceArrayField(ArrayField):
         }
         defaults.update(kwargs)
         return super(ArrayField, self).formfield(**defaults)
+
+
+class JSONSchemaField(JSONField):
+    def __init__(self, *args, **kwargs):
+        self.schema = kwargs.pop("schema", None)
+        super().__init__(*args, **kwargs)
+
+    @property
+    def _schema_data(self):
+        schema_path = Path(settings.BASE_DIR) / self.schema
+        with schema_path.open("r") as _fh:
+            return json.loads(_fh.read())
+
+    def _validate_schema(self, value):
+        # Disable validation when migrations are faked
+        if self.model.__module__ == "__fake__":
+            return True
+        try:
+            status = validate(value, self._schema_data)
+        except jsonschema_exceptions.ValidationError as exp:
+            raise exceptions.ValidationError(str(exp), code="invalid")
+        return status
+
+    def validate(self, value, model_instance):
+        super().validate(value, model_instance)
+        self._validate_schema(value)
+
+    def pre_save(self, model_instance, add):
+        value = super().pre_save(model_instance, add)
+        if value and not self.null:
+            self._validate_schema(value)
+        return value
