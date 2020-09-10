78ee1a45bfd805293182b36718341bd9d77c188e
Simon Wiles <simonjwiles@gmail.com>
2020-09-10 10:31:08 -0700

    Update module docstring

--- sati/settings.py
+++ sati/settings.py
@@ -1,12 +1,4 @@
-"""
-Django settings for sati project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/3.0/ref/settings/
-"""
+""" SATI :: Django Settings """
 
 import os
 
