9135f92f0c6f497609e839149586ee7ac81ddd1f
Simon Wiles <simonjwiles@gmail.com>
2020-09-10 10:42:12 -0700

    Ensure debug toolbar loads when DEBUG=True

--- sati/settings.py
+++ sati/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get(
 DEBUG = os.environ.get("DEBUG", "true").lower() == "true"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1,0.0.0.0").split(",")
-
+INTERNAL_IPS = ("127.0.0.1",)
 
 # Application definition
 INSTALLED_APPS = [
