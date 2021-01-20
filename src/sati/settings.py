0d643ea565de9f52564d2ee5db47699aaac86ce2
Simon Wiles <simonjwiles@gmail.com>
2021-01-20 14:36:13 -0800

    Add static/ to the repo
    Contents are ignored, but the "empty dir" is committed.

--- src/sati/settings.py
+++ src/sati/settings.py
@@ -114,7 +114,7 @@ MEDIA_URL = os.getenv("MEDIA_URL", "/media/")
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.getenv("STATIC_ROOT", os.path.join(BASE_DIR, "static"))
+STATIC_ROOT = os.getenv("STATIC_ROOT", os.path.join(BASE_DIR, "../static"))
 STATIC_URL = os.getenv("STATIC_URL", "/static/")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "assets")]
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
