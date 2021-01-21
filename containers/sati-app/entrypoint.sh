3184a6a6505f8e582cc9e90ee0857f3b2b701156
Simon Wiles <simonjwiles@gmail.com>
2021-01-21 13:08:37 -0800

    Wait for postgres service to be available before init-ing DB
    There are lots of ways to do this, but alpine/ash ships with busybox which has a built-in implementation of `nc`, and this is adequate for present needs.

--- containers/sati-app/entrypoint.sh
+++ containers/sati-app/entrypoint.sh
@@ -4,9 +4,14 @@ set -eou pipefail;
 
 cmd="$*";
 
+while ! nc -z "${DB_HOST}" "${DB_PORT}";
+do
+  echo 'Waiting for postgres...';
+  sleep 5;
+done;
+
 cd /opt/sati;
 python manage.py migrate;
 python manage.py collectstatic --no-input -v0;
 
-
-exec $cmd
+exec $cmd;
